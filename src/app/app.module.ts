bc6a58b3a7b63a48e498d1e7cfe346622358f765
zhixua <cao.zhix@northeastern.edu>
2020-12-03 17:13:17 -0800

    Assignment7

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
+import {FormsModule} from '@angular/forms';
+import {CourseService} from '../services/course-service';
+import {ModuleService} from '../services/ModuleService';
+import { CourseListComponent } from './course-list/course-list.component';
+import { ModuleListComponent } from './module-list/module-list.component';
+import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
+import { TopicPillsComponent } from './topic-pills/topic-pills.component';
+import { WidgetListComponent } from './widget-list/widget-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseNavigatorComponent,
+    CourseListComponent,
+    ModuleListComponent,
+    LessonTabsComponent,
+    TopicPillsComponent,
+    WidgetListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    CourseService, ModuleService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
